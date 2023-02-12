78f14605d3825bce94f3db492b4d054b40906de6
Dung Nguyen <bluesky.1289@gmail.com>
2023-02-12 15:45:39 +0700

    Add url config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/",
+    site: "https://bluzky.github.io/",
+    base: "/tangerine",
   integrations: [
     tailwind({
       config: {
